25f0db8d091e563db2f590e7368c0cb5f14f62fb
shubhitech <shubhi171197gupta@gmail.com>
2023-09-18 19:35:18 +0530

    second commit

--- DemoApplication/src/in/ineuron/cl/DemoApplication.java
+++ DemoApplication/src/in/ineuron/cl/DemoApplication.java
@@ -13,6 +13,11 @@ public class DemoApplication {
 	System.out.println("the length of the string ::" +count);
 	System.out.println("done");
 	System.out.println("all");
+	
+	StringBuffer sb = new StringBuffer();
+	 int ct =sb.append("plz").reverse().length();
+	 System.out.println(ct);
+	 System.out.println("printed");
 		
 		
 
